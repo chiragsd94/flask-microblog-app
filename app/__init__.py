b4117f2d3b8f5c79dbcac253a8ae3cd0bd647311
Chirag SD <s.chirag.danavendra@gmail.com>
2025-07-25 15:28:16 +0530

    initial commit and added app configurations

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+import os
+from flask import Flask
+from dotenv import load_dotenv
+from app.extension import db, migrate
+
+load_dotenv()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+    db.init_app(app)
+    migrate.init_app(app, db)
+    return app
